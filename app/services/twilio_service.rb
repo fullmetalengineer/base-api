745e06c4822d629f769b08b736351be807a8b42d
Alan Long <alan.d.long@outlook.com>
2020-04-27 14:38:20 -0500

    Rename TwilioService's action's name

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -1,7 +1,7 @@
 class TwilioService
     require 'twilio-ruby'
   
-    def self.send_text_message(to, message)
+    def self.text(to, message)
         # Fire off the worker
         TwilioWorker.perform_async(to, message)
     end
