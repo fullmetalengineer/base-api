8bcc6f0378a226e9482163e47774783de80ef0b7
Alan Long <alan.d.long@outlook.com>
2020-04-27 15:19:38 -0500

    Upgrade to rails 6 (#1)
    * Files upgraded that don't need modifications
    * Additional files that need modifications
    * New framework defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,29 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  resources :users, only: [:create] do
-    collection do
-      post 'login'
-      get 'me'
-      delete 'logout'
+  require 'sidekiq/web'
+  scope :monitoring do
+    # Sidekiq Basic Auth from routes on production environment
+    if Rails.env.production?
+      Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_AUTH_USERNAME'])) &
+          ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_AUTH_PASSWORD']))
+      end
     end
+
+    mount Sidekiq::Web, at: '/sidekiq'
   end
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      namespace :users do
+        post :login
+        post :signup
+        get :me
+        delete :logout
+        post :update_password
+        post :password_reset
+        post :confirm_password_reset
+      end
+    end
+  end
 end
