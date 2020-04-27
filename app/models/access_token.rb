2188f208f09a0fa95e74c74b75bfd8e63ce39da5
Alan Long <alan.d.long@outlook.com>
2020-04-27 14:41:03 -0500

    Update naming

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -7,7 +7,7 @@ class AccessToken
   end
 
   def generate(obj="")
-    Digest::SHA1.hexdigest("#{@model.to_s}-#{@model.id}-#{@model.created_at}-[nerd_party-#{@model.id.to_s(36)}]-[#{obj}]-[#{rand(-200..200).days.ago}]")
+    Digest::SHA1.hexdigest("#{@model.to_s}-#{@model.id}-#{@model.created_at}-[basic_api-#{@model.id.to_s(36)}]-[#{obj}]-[#{rand(-200..200).days.ago}]")
   end
 
 end
