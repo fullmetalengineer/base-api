49155caf3aef1a141705527f531b2755f8b9c9c4
Alan Long <alan.d.long@outlook.com>
2020-04-28 13:43:34 -0500

    Add unit tests to the user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,7 +20,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that each user must have a unique email" do
+    alan_user = users(:alan)
+    zach_user = users(:zach)
+
+    alan_user.email = zach_user.email
+    
+    assert_not alan_user.save, "Was able to create a non-unique email in the database on a user"
+    assert alan_user.errors.keys.include?(:email), "Saving a user with a non-unique email did not generate a model error"
+  end
+
+  test "that calling generate_token! updates the user's token value in the database" do
+    alan_user = users(:alan)
+    alan_user.update(token: nil)
+    assert alan_user.token.nil?, "Did not nullify the user's token"
+    
+    alan_user.generate_token!
+    assert_not alan_user.token.blank?, "Failed to generate a token"
+  end
+
+  test "that getting a user profile returns the proper keys" do
+    alan_user = users(:alan)
+    expected_keys = [:first_name, :last_name, :email, :token] 
+    assert alan_user.profile.keys.size == 4, "Generated the wrong number of attributes on the user profile"
+    assert alan_user.profile.keys - expected_keys == [], "Did not get the proper keys back"
+  end
 end
