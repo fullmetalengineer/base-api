fc2c9d7e480c74718cc424dac0cba82ceaa1d880
Alan Long <alan.d.long@outlook.com>
2020-04-28 21:00:52 -0500

    Move Twilio account_sid and auth_token to initializer

--- app/workers/twilio_worker.rb
+++ app/workers/twilio_worker.rb
@@ -5,17 +5,13 @@ class TwilioWorker
   
     def perform(to, message)
         # Set up the required environment variables
-        account_sid = ENV['TWILIO_ACCOUNT_SID']
-        auth_token = ENV['TWILIO_AUTH_TOKEN']
         twilio_phone_number = ENV['TWILIO_DEFAULT_PHONE_NUMBER']
 
         # Raise errors if environment variables not set
-        raise "Environment variable TWILIO_ACCOUNT_SID not set" unless account_sid
-        raise "Environment variable TWILIO_AUTH_TOKEN not set" unless auth_token
         raise "Environment variable TWILIO_DEFAULT_PHONE_NUMBER not set" unless twilio_phone_number
 
         # Send the text
-        client = Twilio::REST::Client.new account_sid, auth_token
+        client = Twilio::REST::Client.new
         message = client.messages.create(from: twilio_phone_number.to_s, to: to.to_s, body: message)
         puts message.sid
     end
---
+++ config/initializers/twilio.rb
@@ -0,0 +1,7 @@
+Twilio.configure do |config|
+    config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+    config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+    # raise "Environment variable TWILIO_ACCOUNT_SID not set" unless ENV['TWILIO_ACCOUNT_SID']
+    # raise "Environment variable TWILIO_AUTH_TOKEN not set" unless ENV['TWILIO_AUTH_TOKEN']
+end
+  
