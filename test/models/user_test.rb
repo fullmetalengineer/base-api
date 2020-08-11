288332b3b5bc78aadda9af0694748d178ad861ed
Alan Long <alan.d.long@outlook.com>
2020-08-11 12:41:02 -0500

    Add User unit tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -45,4 +45,60 @@ class UserTest < ActiveSupport::TestCase
     assert alan_user.profile.keys.size == 4, "Generated the wrong number of attributes on the user profile"
     assert alan_user.profile.keys - expected_keys == [], "Did not get the proper keys back"
   end
+
+  test "that creating a user works" do
+    result = UserService.create({
+      first_name: "test",
+      last_name: "account",
+      email: "test@test.com",
+      password: "MyPassword"
+    })
+
+    assert result.success? == true
+    assert result.payload.class.name.to_sym == :User
+  end
+
+  test "that updating a user works" do
+    result = UserService.create({
+      first_name: "test",
+      last_name: "account",
+      email: "test@test.com",
+      password: "MyPassword"
+    })
+
+    result2 = UserService.self_update(result.payload, { id: result.payload.id, first_name: "Jeff", last_name: "Portnoy", email: "test2@test.com"})
+    assert result2.success?
+    assert result2.payload.first_name == "Jeff"
+    assert result2.payload.last_name == "Portnoy"
+  end
+
+  test "logging in a user works" do
+    result = UserService.create({
+      first_name: "test",
+      last_name: "account",
+      email: "test@test.com",
+      password: "MyPassword"
+    })
+
+    login_result = UserService.login(result.payload.email, "MyPassword")
+    assert login_result.success?
+    assert login_result.payload.class.name.to_sym == :User
+  end
+
+  test "logging out a user works" do
+    result = UserService.create({
+      first_name: "test",
+      last_name: "account",
+      email: "test@test.com",
+      password: "MyPassword"
+    })
+
+    login_result = UserService.login(result.payload.email, "MyPassword")
+    assert login_result.success?
+    assert login_result.payload.class.name.to_sym == :User
+
+    logout_result = UserService.logout(login_result.payload)
+    assert logout_result.success?
+    assert logout_result.payload.nil?
+  end
 end
