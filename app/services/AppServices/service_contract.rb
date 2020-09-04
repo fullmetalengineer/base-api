690be8bbdaaad72bd092b09761078256e0ab40fb
Alan Long <alan.d.long@outlook.com>
2020-09-04 17:51:21 -0500

    Comments for SolarGraph

--- app/services/AppServices/service_contract.rb
+++ app/services/AppServices/service_contract.rb
@@ -3,10 +3,12 @@
 module AppServices
   # Standardizes what a service method should always return
   module ServiceContract
+    # Create the contractual return object
     def self.sign(success:, payload:, errors:)
       OpenStruct.new({ success?: success, payload: payload, errors: convert_to_custom_error(errors) })
     end
 
+    # Convert a string, an array of strings, or a model's errors to a CustomError instance
     def self.convert_to_custom_error(errors)
       case errors.class.name.to_sym
       when :String, :NilClass
